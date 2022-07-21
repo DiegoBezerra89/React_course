c64ca2ab0ae09f62d7361befa5db03792665f7f0
DiegoBezerra89 <diegobmartins21@gmail.com>
2022-07-21 08:22:43 -0300

    Finished context module

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,19 +1,32 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 import ProductCard from "../../components/product-card/product-card.component";
+
 import { CategoriesContext } from "../../contexts/categories.context";
 
-const CategoryPage = () => {
-  const { categoryProducts } = useContext(CategoriesContext);
+import "./category.styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
   return (
-    <>
-      <div className="products-container">
-        {categoryProducts?.map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
       </div>
-    </>
+    </Fragment>
   );
 };
 
-export default CategoryPage;
+export default Category;
